b2b6d5529e02ecc6a4e044028f66198adef7e79c
nitinbisht122003 <97455534+nitinbisht122003@users.noreply.github.com>
2022-11-01 01:24:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -87,12 +87,102 @@ const next = () => {
 };
 
 next();
-const link = document.querySelector(".link");
-const transition = document.querySelector(".transition");
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-  transition.classList.add("slide");
-  setTimeout(() => {
-    window.location = link.href;
-  }, 900);
-});
+
+
+var container = document.getElementById("array");
+
+
+function generatearray() {
+	for (var i = 0; i < 20; i++) {
+
+		
+		var value = Math.ceil(Math.random() * 100);
+
+		
+		var array_ele = document.createElement("div");
+
+	
+		array_ele.classList.add("block");
+
+		
+		array_ele.style.height = `${value * 3}px`;
+		array_ele.style.transform = `translate(${i * 30}px)`;
+
+		
+		var array_ele_label = document.createElement("label");
+		array_ele_label.classList.add("block_id");
+		array_ele_label.innerText = value;
+
+		
+		array_ele.appendChild(array_ele_label);
+		container.appendChild(array_ele);
+	}
+}
+
+
+function swap(el1, el2) {
+	return new Promise((resolve) => {
+
+		
+		var temp = el1.style.transform;
+		el1.style.transform = el2.style.transform;
+		el2.style.transform = temp;
+
+		window.requestAnimationFrame(function() {
+
+			
+			setTimeout(() => {
+				container.insertBefore(el2, el1);
+				resolve();
+			}, 250);
+		});
+	});
+}
+
+
+async function BubbleSort(delay = 100) {
+	var blocks = document.querySelectorAll(".block");
+
+	// BubbleSort Algorithm
+	for (var i = 0; i < blocks.length; i += 1) {
+		for (var j = 0; j < blocks.length - i - 1; j += 1) {
+
+			// To change background-color of the
+			// blocks to be compared
+			blocks[j].style.backgroundColor = "#a53db7";
+			blocks[j + 1].style.backgroundColor = "#a53db7";
+
+			// To wait for .1 sec
+			await new Promise((resolve) =>
+				setTimeout(() => {
+					resolve();
+				}, delay)
+			);
+
+			console.log("run");
+			var value1 = Number(blocks[j].childNodes[0].innerHTML);
+			var value2 = Number(blocks[j + 1]
+						.childNodes[0].innerHTML);
+
+			// To compare value of two blocks
+			if (value1 > value2) {
+				await swap(blocks[j], blocks[j + 1]);
+				blocks = document.querySelectorAll(".block");
+			}
+
+			// Changing the color to the previous one
+			blocks[j].style.backgroundColor = "#c0a6f6";
+			blocks[j + 1].style.backgroundColor = "#c0a6f6";
+		}
+
+		//changing the color of greatest element
+		//found in the above traversal
+		blocks[blocks.length - i - 1]
+				.style.backgroundColor = "#86377b";
+	}
+}
+
+generatearray();
+
+BubbleSort();
+
